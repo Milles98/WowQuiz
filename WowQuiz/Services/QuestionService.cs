11f41fa6b38db0f37b086ea2da1f81593af3e185
Mille Elfver <mille.elfver98@gmail.com>
2024-06-28 10:40:03 +0200

    Refactor QuizViewModel and add QuestionService
    - Updated QuizViewModel.cs to make CheckAnswer method asynchronous and simplified its logic for checking answers. Now uses a boolean `isCorrect` for result determination and displays feedback using an asynchronous call to `Shell.Current.DisplayAlert`.
    - Added a new method `MoveToNextQuestion` in QuizViewModel.cs for navigating to the next quiz question.
    - Introduced IQuestionService interface under WowQuiz.Services namespace, defining a contract for fetching quiz questions asynchronously.
    - Implemented QuestionService class, fulfilling the IQuestionService contract by asynchronously returning a hard-coded list of World of Warcraft themed questions.

---
+++ WowQuiz/Services/QuestionService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WowQuiz.Models;
+
+namespace WowQuiz.Services
+{
+    public class QuestionService : IQuestionService
+    {
+        public async Task<List<Question>> GetQuestionsAsync()
+        {
+            return new List<Question>
+            {
+                new Question
+                {
+                    QuestionText = "What is the capital city of the Night Elves?",
+                    Answers = new List<string> {"Darnassus", "Stormwind", "Orgrimmar", "Silvermoon"},
+                    CorrectAnswerIndex = 0
+                },
+                new Question
+                {
+                    QuestionText = "Who was the Lich King before Arthas?",
+                    Answers = new List<string> {"Ner'zhul", "Illidan Stormrage", "Bolvar Fordragon", "Kil'jaeden"},
+                    CorrectAnswerIndex = 0
+                }
+            };
+        }
+    }
+}
