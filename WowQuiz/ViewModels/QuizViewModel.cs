acb3327892b70af632d323c77f4a4043537125f1
Mille Elfver <mille.elfver98@gmail.com>
2024-06-28 10:13:22 +0200

    Refactor UI and implement quiz functionality
    - Overhauled MainPage.xaml UI, removing old ScrollView and adding a simpler layout with a "startButton" to initiate the quiz.
    - Updated MainPage.xaml.cs event handlers, replacing `OnCounterClicked` with `OnStartButtonClicked` for quiz page navigation.
    - Enhanced WowQuiz.csproj with `CommunityToolkit.Mvvm` package for MVVM pattern adoption and added a "Views" folder for better organization.
    - Introduced a `Question` model in WowQuiz.Models for representing quiz questions.
    - Created `QuizViewModel` in WowQuiz.ViewModels with observable properties, an ObservableCollection for questions, and a `CheckAnswer` command for interactivity.
    These changes pivot the app towards quiz functionality, utilizing MVVM for improved structure and user experience.

---
+++ WowQuiz/ViewModels/QuizViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WowQuiz.Models;
+
+namespace WowQuiz.ViewModels
+{
+    public partial class QuizViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private Question _currentQuestion;
+
+        [ObservableProperty]
+        private string _feedbackMessage;
+
+        public ObservableCollection<Question> Questions { get; } = new ObservableCollection<Question>();
+
+        public QuizViewModel()
+        {
+            //ladda frågorna här
+        }
+
+        [RelayCommand]
+        public void CheckAnswer(int selectIndex)
+        {
+            if (selectIndex == CurrentQuestion.CorrectAnswerIndex)
+            {
+                FeedbackMessage = "Correct!";
+            }
+            else
+            {
+                FeedbackMessage = "False!";
+            }
+        }
+    }
+}
