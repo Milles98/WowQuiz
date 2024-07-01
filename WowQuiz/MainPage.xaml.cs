f1fc8f0f1df8303c2b63bbfbb20c7c8224be75ba
Mille Elfver <mille.elfver98@gmail.com>
2024-07-01 13:08:21 +0200

    Refactor mainpage to include username and profile btn so that logged in users can in the future view their information and much more. Also added profilepage

--- WowQuiz/MainPage.xaml.cs
+++ WowQuiz/MainPage.xaml.cs
@@ -1,20 +1,28 @@
-﻿using WowQuiz.Views;
+﻿using WowQuiz.ViewModels;
+using WowQuiz.Views;
 
 namespace WowQuiz
 {
     public partial class MainPage : ContentPage
     {
         private readonly IServiceProvider _serviceProvider;
-        public MainPage(IServiceProvider serviceProvider)
+        public MainPage(IServiceProvider serviceProvider, MainViewModel viewModel)
         {
             _serviceProvider = serviceProvider;
             InitializeComponent();
+            BindingContext = viewModel;
         }
 
         private async void OnStartButtonClicked(object sender, EventArgs e)
         {
             var quizPage = _serviceProvider.GetRequiredService<QuizPage>();
             await Navigation.PushAsync(quizPage);
+        }
+        
+        private async void OnProfileButtonClicked(object sender, EventArgs e)
+        {
+            var profilePage = _serviceProvider.GetRequiredService<ProfilePage>();
+            await Navigation.PushAsync(profilePage);
         }
     }
 
